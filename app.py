7c04a009d5dda27e45e27152579bdfedec6cac35
sohanactecal <sohanactecal@gmail.com>
2023-02-22 18:34:05 +0530

    some changes in  pages

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, session
 import json
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-
+app.secret_key = 'your secret key'
 database_conn= open("config.json", 'r')
 configuration= json.load(database_conn)
 app.config['MYSQL_HOST'] = configuration['host']
@@ -15,7 +15,7 @@ app.config['MYSQL_DB'] = configuration['database']
 mysql = MySQL(app)
 
 
-
+# -----------------student registration -----------------------------
 @app.route('/add-student',methods=['GET','POST'])
 def storeData():
     cur = mysql.connection.cursor()
@@ -27,7 +27,7 @@ def storeData():
         r_no=request.form['registration_no']
         course_name=request.form['course_name']
         date= request.form['date']
-        duration= request.form['duration']
+        duration= request.form['duration']  
         mobile_no= request.form['mobile_no']
         email=request.form ['email']
         address= request.form['address']
@@ -37,24 +37,46 @@ def storeData():
             
     return render_template("form.html")
 
-
+#----------------student detail---------------
 @app.route('/')
 def studentDetail():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * from signin_detail")
-    result = cur.fetchall()
-
-
-
-    
+    result = cur.fetchall()  
     return render_template("detail.html", result=result)
-
+#-----------fee------------
 @app.route('/fee')
 def fee():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * from signin_detail")
     result = cur.fetchall()
     return render_template("fee.html", result=result)
+
+#-------------admin login--------------------- 
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'user_name' in request.form and 'password' in request.form:
+        username = request.form['user_name']
+        password = request.form['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM admin_login WHERE user_name = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True 
+           
+            return render_template('login.html',mag=msg)
+            
+        else:
+            msg="login Successfully"
+            return render_template('login.html',mag=msg)
+    return render_template('login.html')
+ 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
  
 if __name__ == '__main__':
 	app.run(debug=True)
